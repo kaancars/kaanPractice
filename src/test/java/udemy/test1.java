6381a8ab83f693e7fa7731f9d2c8afb35f93e9f1
kaancars <111206375+kaancars@users.noreply.github.com>
2022-08-13 19:24:16 -0400

    Update test1.java
    name changed

--- src/test/java/udemy/test1.java
+++ src/test/java/udemy/test1.java
@@ -2,7 +2,7 @@ package udemy;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("Kaan cars");
+        System.out.println("Mustafa Tanis");
 
 
     }
